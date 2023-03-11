ba18dc21fe5e7d877f1f70a83c8c8a2120021559
Rolando Gonzalez <rolygonz89@gmail.com>
2023-03-11 15:11:53 -0500

    DTOs

--- src/app/components/employe.model.ts
+++ src/app/components/employe.model.ts
@@ -11,4 +11,37 @@ export interface EmployeModel{
   suspended: boolean,
   email:string
 }
+export interface IEmployeCreateDTO{
+  id:number,
+  name:string,
+  lastName:string,
+  age:number,
+  work:string,
+  roll:string,
+  gender: Genderenum,
+  suspended: boolean,
+  email:string
+}
+export interface IEmployeGetDTO{
+  id:number,
+  name:string,
+  lastName:string,
+  age:number,
+  work:string,
+  roll:string,
+  gender: Genderenum,
+  suspended: boolean,
+  email:string
+}
+export interface IEmployeUpdateDTO{
+  id:number,
+  name:string,
+  lastName:string,
+  age:number,
+  work:string,
+  roll:string,
+  gender: Genderenum,
+  suspended: boolean,
+  email:string
+}
 
